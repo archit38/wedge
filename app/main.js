156a7af7a4fd75c4a23c91cd01beb41c54d1327f
archit <architbhardwaj88@gmail.com>
2015-10-15 22:44:08 +0100

    js files

---
+++ app/main.js
@@ -0,0 +1,16 @@
+﻿var MyApp = angular.module('Wedge', ['ng', 'ngResource']);
+
+MyApp.factory('flickrPhotos', function ($resource) {
+    return $resource('http://api.flickr.com/services/feeds/photos_public.gne', { format: 'json', jsoncallback: 'JSON_CALLBACK' }, { 'load': { 'method': 'JSONP' } });
+});
+
+
+
+MyApp.controller('WedgeCtrl', function ($scope, flickrPhotos) {
+    $scope.tag = "Nature";
+    $scope.loadPhotos = function(){
+        $scope.photos = flickrPhotos.load({ tags: $scope.tag });
+    }
+    $scope.loadPhotos();
+});
+   
