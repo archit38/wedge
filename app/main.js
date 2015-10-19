6e44c29db1784f57715297b7c69a0a89790d6d60
archit <architbhardwaj88@gmail.com>
2015-10-19 23:39:20 +0100

    instagram

--- app/main.js
+++ app/main.js
@@ -1,19 +1,58 @@
-﻿var MyApp = angular.module('Wedge', ['ng', 'ngResource']);
+var MyApp = angular.module('Wedge', ['ng', 'ngResource']);
 
 MyApp.factory('flickrPhotos', function ($resource) {
     return $resource('http://api.flickr.com/services/feeds/photos_public.gne', { format: 'json', jsoncallback: 'JSON_CALLBACK' }, { 'load': { 'method': 'JSONP' } });
 });
 
+MyApp.factory('instagram', ['$http',
+    function($http) {
+        var base = "https://api.instagram.com/v1";
+        // get your own client id http://instagram.com/developer/
+        var clientId = 'ca16c8a77172476289b1e85fc8ca6f50';
+        return {
+            'get': function(count, hashtag) {
+                var request = '/tags/' + hashtag + '/media/recent';
+                var url = base + request;
+                var config = {
+                    'params': {
+                        'client_id': clientId,
+                        'count': count,
+                        'callback': 'JSON_CALLBACK'
+                    }
+                };
+                return $http.jsonp(url, config);
+            }
+        };
+    }
+]);
 
-
-MyApp.controller('WedgeCtrl', function ($scope, flickrPhotos) {
+MyApp.controller('WedgeCtrl', function ($scope, flickrPhotos,instagram) {
     $scope.tag = "kittens";
-    $scope.loadPhotos = function(){
+   
+
+    $scope.layout = 'grid';
+    
+    $scope.setLayout = function(layout){
+        $scope.layout = layout;
+    };
+    
+    $scope.isLayout = function(layout){
+        return $scope.layout == layout;
+    };
+
+    $scope.pics = [];
+
+    
+
+   $scope.loadPhotos = function(){
         $scope.photos = flickrPhotos.load({ tags: $scope.tag });
+        instagram.get(10, $scope.tag).success(function(response) {
+            $scope.pics = response.data;
+        });
     }
     $scope.keyPressed = function(keyEvent) {
-    	 if (keyEvent.which === 13)
-    	 	 $scope.loadPhotos();
+         if (keyEvent.which === 13)
+             $scope.loadPhotos();
     }
     $scope.loadPhotos();
 });
